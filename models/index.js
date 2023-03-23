568bfc6224f1273bc4a1ff4ca12b108a3e7da9a2
Callum Mackintosh <callum.e.s.mackintosh@gmail.com>
2023-03-23 13:03:28 +1100

    But why male models?

--- models/index.js
+++ models/index.js
@@ -1,3 +1,30 @@
-const User = require('./User');
+const User = require('./user');
+const BlogPost = require('./blogposts');
+const Comment = require('./comments');
 
-module.exports = { User };
+// Define model associations
+User.hasMany(BlogPost, {
+  foreignKey: 'user_id'
+});
+
+BlogPost.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+BlogPost.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+Comment.belongsTo(BlogPost, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, BlogPost, Comment };
