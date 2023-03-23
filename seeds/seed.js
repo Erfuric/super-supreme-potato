a4ae250ade480a55b8043e11aed49287f0a90d78
Callum Mackintosh <callum.e.s.mackintosh@gmail.com>
2023-03-23 19:19:34 +1100

    Heroku seed v2

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,38 +1,38 @@
-// const sequelize = require('../config/connection');
-// const { User, Post, Comment } = require('../models');
-// const userData = require('./userData.json');
-// const postData = require('./postData.json');
-// const commentData = require('./commentData.json');
+const sequelize = require('../config/connection');
+const { User, Post, Comment } = require('../models');
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+const commentData = require('./commentData.json');
 
-// const seedDatabase = async () => {
-//   await sequelize.sync({ force: true });
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
 
-//   const users = await User.bulkCreate(userData, {
-//     individualHooks: true,
-//     returning: true
-//   });
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true
+  });
 
-//   const posts = await Post.bulkCreate(
-//     postData.map((post) => {
-//       return {
-//         title: post.title,
-//         content: post.content,
-//         user_id: post.user_id
-//       };
-//     })
-//   );
+  const posts = await Post.bulkCreate(
+    postData.map((post) => {
+      return {
+        title: post.title,
+        content: post.content,
+        user_id: post.user_id
+      };
+    })
+  );
 
-//   await Comment.bulkCreate(
-//     commentData.map((comment) => {
-//       return {
-//         content: comment.content,
-//         user_id: comment.user_id,
-//         post_id: comment.post_id
-//       };
-//     })
-//   );
+  await Comment.bulkCreate(
+    commentData.map((comment) => {
+      return {
+        content: comment.content,
+        user_id: comment.user_id,
+        post_id: comment.post_id
+      };
+    })
+  );
 
-//   process.exit(0);
-// };
+  process.exit(0);
+};
 
-// seedDatabase();
+seedDatabase();
