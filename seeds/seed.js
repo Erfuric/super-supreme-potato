01e8d879a1b19d3dc11c2b300d23e5656d5eecef
Callum Mackintosh <callum.e.s.mackintosh@gmail.com>
2023-03-23 13:07:50 +1100

    Whole lot of seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,1 +1,37 @@
-Placeholder
+const sequelize = require('./config/connection');
+const { User, BlogPost, Comment } = require('./models');
+const userData = require('./seeds/userData.json');
+const blogpostData = require('./seeds/blogpostData.json');
+const commentData = require('./seeds/commentData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true
+  });
+
+  const blogposts = await BlogPost.bulkCreate(
+    blogpostData.map((post) => {
+      return {
+        ...post,
+        user_id: users[Math.floor(Math.random() * users.length)].id
+      };
+    })
+  );
+
+  await Comment.bulkCreate(
+    commentData.map((comment) => {
+      return {
+        ...comment,
+        user_id: users[Math.floor(Math.random() * users.length)].id,
+        post_id: blogposts[Math.floor(Math.random() * blogposts.length)].id
+      };
+    })
+  );
+
+  process.exit(0);
+};
+
+seedDatabase();
