a6e1430037cee08a8259c4889aff9de3a72dcaab
Callum Mackintosh <callum.e.s.mackintosh@gmail.com>
2023-03-23 16:54:36 +1100

    Why no work online

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,20 @@
-const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize;
+const Sequelize = require('sequelize');
 
 if (process.env.JAWSDB_URL) {
-  // For production environment (Heroku), use JawsDB connection
-  sequelize = new Sequelize(process.env.JAWSDB_URL, {
-    dialect: 'mysql',
-    logging: false
-  });
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  // For development environment, use local MySQL connection
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    logging: false
-  });
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
 }
 
 module.exports = sequelize;
